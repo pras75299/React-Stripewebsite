76f33e4df4d300b9c403d28616e64eff1eccdf8e
pras75299 <singh.prashantking@gmail.com>
2021-05-24 21:57:08 +0530

    setup context js

--- src/context.js
+++ src/context.js
@@ -1,2 +1,42 @@
-import React, { useState, useContext } from 'react'
-import sublinks from './data'
+import React, { useState, useContext } from 'react';
+import sublinks from './data';
+
+const AppContext = React.createContext();
+
+export const AppProvider = ({children}) => {
+    const[isSidebarOpen, setIsSidebarOpen] = useState(true);
+    const[isSubMenuOpen, setIsSubMenuOpen] = useState(true);
+    
+    const openSidebar = () => {
+        setIsSidebarOpen(true);
+    }
+
+    const closeSidebar = () => {
+        setIsSidebarOpen(false);
+    }
+
+    const openSubMenu = () => {
+        setIsSubMenuOpen(true);
+    }
+
+    const closeSubMenu = () => {
+        setIsSubMenuOpen(false);
+    }
+
+
+    return <AppContext.Provider value={{
+        isSidebarOpen,
+        isSubMenuOpen,
+        openSidebar,
+        closeSidebar,
+        openSubMenu,
+        closeSubMenu
+    }}>
+        {children}
+    </AppContext.Provider>
+}
+
+
+export const useGlobalContext = () => {
+    return useContext(AppContext)
+}
