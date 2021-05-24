a69c5929593f48a75c7f15dd7376d8fc485d5e8a
pras75299 <singh.prashantking@gmail.com>
2021-05-24 22:43:31 +0530

    setup hero component

--- src/Hero.js
+++ src/Hero.js
@@ -1,8 +1,25 @@
 import React from 'react'
 import phoneImg from './images/phone.svg'
-
+import {useGlobalContext} from './context';
 const Hero = () => {
-  return <h2>hero component</h2>
+  const {closeSubMenu} = useGlobalContext();
+  return <section className="hero">
+    <div className="hero-center">
+      <article className="hero-info">
+        <h1>Payments infrastructure for the internet</h1> 
+        <p>
+          lorem ipsum dolor sit amet, consectetur adip
+          lorem ipsum dolor sit amet, consectetur adip
+          lorem ipsum dolor sit amet, consectetur adip
+          lorem ipsum dolor sit amet, consectetur adip
+          lorem ipsum dolor sit amet, consectetur adip
+        </p>
+      </article>
+      <article className="hero-images">
+        <img src={phoneImg} alt="phone-img" className="phone-img"/>
+      </article>
+    </div>
+  </section>
 }
 
 export default Hero
