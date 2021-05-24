ec3eb8927735184226d14c1ee3cf7c55b71ff05d
pras75299 <singh.prashantking@gmail.com>
2021-05-24 22:38:34 +0530

    setup navbar component

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,9 +1,31 @@
 import React from 'react'
 import logo from './images/logo.svg'
 import { FaBars } from 'react-icons/fa'
-
-const Navbar = () => {
-  return <h2>navbar component</h2>
+import {useGlobalContext} from './context';
+const Navbar = () => {  
+  const {openSidebar, openSubMenu, closeSubMenu} = useGlobalContext();
+  return <nav className="nav">
+    <div className="nav-center">
+      <div className="nav-header">
+        <img src={logo} className="nav-logo" alt="stripe" />
+        <button className="btn toggle-btn" onClick={openSidebar}>
+          <FaBars/>
+        </button>
+      </div>
+      <ul className="nav-links">
+        <li>
+          <button className="link-btn">products</button>
+        </li>
+        <li>
+          <button className="link-btn">developers</button>
+        </li>
+        <li>
+          <button className="link-btn">company</button>
+        </li>
+      </ul>
+      <button className="btn signin-btn">Sign in</button>
+    </div>
+  </nav>
 }
 
 export default Navbar
